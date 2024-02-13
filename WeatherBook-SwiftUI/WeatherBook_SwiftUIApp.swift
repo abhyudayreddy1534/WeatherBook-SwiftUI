c3552010b138783fe6316b63a59162c315a3e1a1
Abhyuday Reddy Nandikonda <abhyudayreddy1534@gmail.com>
2024-02-13 16:55:09 -0600

    Initial Commit

---
+++ WeatherBook-SwiftUI/WeatherBook_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherBook_SwiftUIApp.swift
+//  WeatherBook-SwiftUI
+//
+//  Created by Sravanthi Chinthireddy on 13/02/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherBook_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
